89b842a99b0e1a1688da10af947c41e132611dd9
Fritz Lin <uxfritz@163.com>
2016-10-03 13:14:04 +0800

    ScrollView refactor

--- app/App.js
+++ app/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react'
 import {
-  Animated, Easing,
+  Keyboard,
   ScrollView, StyleSheet,
   Text, TextInput, View,
 } from 'react-native'
 import Spacer from 'react-native-keyboard-spacer'
 import Message from './components/Message'
 import ws from './ws'
-import Dimensions from 'Dimensions'
 import _ from 'lodash'
 
+const noop = () => {}
 const user = {
   id: 'fritz.lin',
   name: 'Fritz',
 }
-const BarHeight = 20
-const windowHeight = Dimensions.get('window').height - BarHeight
-const scrollHeight = windowHeight - 40
 
 export default class App extends Component {
   constructor () {
     super()
     this.state = {
-      messages: _.times(20, _.constant({
+      messages: _.times(40, _.constant({
         user: {
           id: 'royson.huang',
           name: 'Royson',
@@ -30,33 +27,111 @@ export default class App extends Component {
         text: 'Hello!'
       })),
       text: 'aaaa',
-      visibleHeight: new Animated.Value(windowHeight),
     }
-    this.updateScrollView = this.updateScrollView.bind(this)
+
+    this.contentHeight = null
+    this.scrollHeight = null
+    this.scrollY = null
+
+    // self binding
+    ;[
+      'handleKeyboardShow', 'handleKeyboardHide',
+      'handleScroll', 'handleLayout', 'handleContentChange',
+      'handleTextChange', 'handleSubmit',
+      'handleMessage',
+    ].forEach((method) => {
+      this[method] = this[method].bind(this)
+    })
   }
 
   componentDidMount () {
-    ws.onmessage = (e) => {
-      console.log('WebSocket received: ' + e.data)
-
-      // dispatch(receiveMessage(e.data))
-      const newMsg = JSON.parse(e.data)
-      this.setState({
-        messages: this.state.messages.concat(newMsg)
-      })
-    }
+    ws.onmessage = this.handleMessage
 
     setTimeout(() => {
       this.sendMessage({ type: 'serverJOIN' })
     }, 200)
+
+    Keyboard.addListener('keyboardDidShow', this.handleKeyboardShow)
+    Keyboard.addListener('keyboardDidHide', this.handleKeyboardHide)
+  }
+  componentWillUnmount () {
+    ws.onmessage = noop
+    Keyboard.removeListener('keyboardDidShow', this.handleKeyboardShow)
+    Keyboard.removeListener('keyboardDidHide', this.handleKeyboardHide)
   }
 
-  updateScrollView (x, y) {
-    if (y > scrollHeight) {
-      this.refs.scroller.scrollTo({ y: y - scrollHeight })
+  handleKeyboardShow () {
+    const { scrollHeight, contentHeight } = this
+    if (contentHeight > scrollHeight) {
+      this.refs.scroller.scrollTo({ y: contentHeight - scrollHeight })
+    }
+  }
+  handleKeyboardHide () {
+    const { scrollY, scrollHeight, contentHeight } = this
+    if (scrollY > contentHeight - scrollHeight) {
+      this.refs.scroller.scrollTo({ y: 0 })
+    }
+    else 
+    if (contentHeight > scrollHeight) {
+      this.refs.scroller.scrollTo({ y: contentHeight - scrollHeight })
     }
   }
 
+  handleScroll (e) {
+    this.scrollY = e.nativeEvent.contentOffset.y
+    console.log('handleScroll', { scrollY: this.scrollY })
+  }
+  handleLayout (e) {
+    this.scrollHeight = e.nativeEvent.layout.height
+    console.log('handleLayout', { scrollHeight: this.scrollHeight })
+  }
+
+  handleContentChange (w, h) {
+    this.contentHeight = h
+
+    if (this.scrollHeight == null) {
+      setTimeout(() => {
+        this.scrollToBottomIfNecessary()
+      }, 500)
+    }
+    else {
+      this.scrollToBottomIfNecessary()
+    }
+  }
+
+  scrollToBottomIfNecessary () {
+    // todo: range detection
+    this.scrollToBottom()
+  }
+  scrollToBottom () {
+    const { scrollHeight, contentHeight } = this
+    if (scrollHeight == null) {
+      return
+    }
+    if (contentHeight > scrollHeight) {
+      this.refs.scroller.scrollTo({ y: contentHeight - scrollHeight })
+    }
+  }
+
+  handleTextChange (text) {
+    this.setState({ text })
+  }
+  handleSubmit () {
+    const { text } = this.state
+    if (text.trim() === '') return
+    this.sendMessage({ text })
+    this.setState({ text: '' })
+  }
+
+  handleMessage (e) {
+    console.log('WebSocket received: ' + e.data)
+
+    // dispatch(receiveMessage(e.data))
+    const newMsg = JSON.parse(e.data)
+    this.setState({
+      messages: [...this.state.messages, newMsg]
+    })
+  }
   sendMessage (msg) {
     ws.send(JSON.stringify({
       user,
@@ -67,29 +142,28 @@ export default class App extends Component {
   render () {
     return (
       <View style={styles.bg}>
-        <Animated.View style={{ height: this.state.visibleHeight }}>
-          <View style={styles.container}>
-            <View style={styles.msgsView}>
-              <ScrollView ref="scroller"
-                contentContainerStyle={styles.scrollContainer}
-                onContentSizeChange={this.updateScrollView}>
-                {this.state.messages.map((msg, i) => {
-                  return <Message key={i} message={msg} />
-                })}
-              </ScrollView>
-            </View>
-
-            <TextInput style={styles.textInput}
-              onChangeText={(text) => this.setState({text})}
-              onSubmitEditing={() => {
-                this.sendMessage({ text: this.state.text })
-                this.setState({ text: '' })
-              }}
-              value={this.state.text} />
-
-            <Spacer />
+        <View style={styles.container}>
+          <View style={styles.msgsView}>
+            <ScrollView ref="scroller"
+              contentContainerStyle={styles.scrollContainer}
+              scrollEventThrottle={16}
+              onScroll={this.handleScroll}
+              onLayout={this.handleLayout}
+              onContentSizeChange={this.handleContentChange}>
+              {this.state.messages.map((msg, i) => {
+                return <Message key={i} message={msg} />
+              })}
+            </ScrollView>
           </View>
-        </Animated.View>
+
+          <TextInput ref="textInput" style={styles.textInput}
+            onChangeText={this.handleTextChange}
+            onSubmitEditing={this.handleSubmit}
+            blurOnSubmit={false}
+            value={this.state.text} />
+
+          <Spacer />
+        </View>
       </View>
     )
   }
@@ -98,7 +172,8 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   bg: {
     backgroundColor: '#F5FCFF',
-    paddingTop: BarHeight,
+    paddingTop: 20,
+    flex: 1,
   },
   container: {
     flex: 1,
